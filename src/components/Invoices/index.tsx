35ab375f9d18e520acb4c376908f0dd3fbb2067d
Mahkamjon Raupov <devbrovki@gmail.com>
2022-05-29 23:52:17 +0200

    invoices responsive

--- src/components/Invoices/index.tsx
+++ src/components/Invoices/index.tsx
@@ -1,11 +1,15 @@
-import React, { FC } from 'react'
+import { FC } from 'react'
 import { InvoiceProps } from '../../lib/Types';
 import Invoice from './Invoice';
+//  styles
+import styles from '../../styles/css/invoices.module.css';
+
+
 
 const Invoices:FC<{data:InvoiceProps[]}> = ({data}) => {
  const invoices = data.map(invoice => <Invoice key={invoice.id} data={invoice}/>);
   return (
-   <div>
+   <div className={styles.invoices_container}>
      {invoices}
    </div>
   );
